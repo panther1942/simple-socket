96d8ce8c5ec4cecae726421afb69c6e29924a2bc
Erika <954584927@qq.com>
2021-01-15 08:02:02 +0800

    优化来优化去又回到原点了 就现在这个还算效率高的 目前的瓶颈在加密算法这块 四核八线程主频2.49GHz 睿频3.6GHz 本地传输 设置为4线程总速率18.4M/s附近 8线程总速率17.6M/s附近 当然单线程是最慢的 仅有8M/s

--- socket-core/src/main/java/cn/erika/socket/core/tcp/TcpReader.java
+++ socket-core/src/main/java/cn/erika/socket/core/tcp/TcpReader.java
@@ -6,68 +6,80 @@ import cn.erika.socket.exception.DataFormatException;
 import cn.erika.socket.model.pto.DataInfo;
 
 // 根据自定协议实现的一个处理数据的类
-class TcpReader implements Reader{
-    private DataInfo info;
-    private byte[] cache;
+class TcpReader implements Reader {
+    private DataInfo dataInfo;
+    private byte[] cache = new byte[0];
     private int pos = 0;
+    private byte[] temp = null;
 
 
     // 因为一个socket或者channel对应一个reader 所以没必要加锁
-    // 下面负责的逻辑是处理粘包的 因为尝试次数太多 忘了当初是咋想的了 并发数稍高一点就会出问题
-    public synchronized void read(ISocket socket, byte[] data, int len) throws DataFormatException {
-        // 因为数组长度和有效数据的长度很有可能不一致 因此需要按照读取的长度拷贝一次数组
-        byte[] tmp = new byte[len];
-        System.arraycopy(data, 0, tmp, 0, len);
-        while (tmp != null && tmp.length > 0) {
-            if (info == null) {
-                byte[] tmp2 = new byte[pos + tmp.length];
-                if (pos > 0) {
-                    System.arraycopy(cache, 0, tmp2, 0, pos);
-                }
-                System.arraycopy(tmp, 0, tmp2, pos, tmp.length);
-                tmp = getDataInfo(tmp2, tmp2.length);
-                if (info != null) {
-                    cache = new byte[info.getLen()];
-                    pos = 0;
-                } else {
-                    break;
-                }
-            }
-            int available = cache.length - pos;
-            if (tmp.length <= available) {
-                System.arraycopy(tmp, 0, cache, pos, tmp.length);
-                pos += tmp.length;
-                tmp = null;
-            } else {
-                System.arraycopy(tmp, 0, cache, pos, available);
-                pos = cache.length;
-                byte[] tmp2 = new byte[tmp.length - available];
-                System.arraycopy(tmp, available, tmp2, 0, tmp.length - available);
-                tmp = tmp2;
-            }
-            if (pos == cache.length && (pos > 0 || info.getLen() == 0)) {
-                info.setData(cache);
-                socket.receive(info);
-                info = null;
-                cache = null;
-                pos = 0;
-            }
+    // 下面负责的逻辑是处理粘包的
+    public void read(ISocket socket, byte[] data, int len) throws DataFormatException {
+        byte[] tmp;
+        if (temp != null && temp.length > 0) {
+            // 复制上次未处理的数据 目前测试发现这块没有执行
+            tmp = new byte[temp.length + len];
+            System.arraycopy(temp, 0, tmp, 0, temp.length);
+            System.arraycopy(data, 0, tmp, temp.length, len);
+            temp = null;
+        } else {
+            tmp = new byte[len];
+            System.arraycopy(data, 0, tmp, 0, len);
         }
+        while (tmp != null && tmp.length > 0) {
+            if (dataInfo == null) {
+                // 如果数据头为空 tmp的长度大于等于数据头长度 则尝试获取数据头
+                if (tmp.length >= DataInfo.LEN) {
+                    // 将剩下的数据放在tmp中等待读取
+                    // 如果获取数据头失败则抛出异常
+                    tmp = getInfo(tmp, tmp.length);
+                    cache = new byte[dataInfo.getLen()];
+                } else {
+                    // 如果tmp的长度比数据头短 则放在缓存等待下一次添加数据
+                    temp = tmp;
+                    tmp = null;
+                }
+            } else {
+                int available = dataInfo.getLen() - pos;
+                // 如果tmp长度比缓存剩余长度大 则能放多少放多少 剩下的放tmp里面
+                if (tmp.length > available) {
+                    System.arraycopy(tmp, 0, cache, pos, available);
+                    pos += available;
+                    temp = new byte[tmp.length - available];
+                    System.arraycopy(tmp, available, temp, 0, temp.length);
+                    tmp = temp;
+                    temp = null;
+                } else {
+                    // 如果剩下的缓存刚好能放下 则都塞进去
+                    System.arraycopy(tmp, 0, cache, pos, tmp.length);
+                    pos += tmp.length;
+                    tmp = null;
+                }
+                // 如果pos等于数据头中的长度 则处理数据
+                if (pos == dataInfo.getLen()) {
+                    dataInfo.setData(cache);
+                    socket.receive(dataInfo);
+                    dataInfo = null;
+                    cache = null;
+                    pos = 0;
+                }
+            }
+
+        }
+
     }
 
-    private byte[] getDataInfo(byte[] data, int len) throws DataFormatException {
-        if (len == 0 || len < DataInfo.LEN) {
-            return data;
-        }
+    private byte[] getInfo(byte[] data, int len) throws DataFormatException {
         byte[] bHead = new byte[DataInfo.LEN];
         System.arraycopy(data, 0, bHead, 0, DataInfo.LEN);
         String strHead = new String(bHead);
         try {
-            info = new DataInfo();
+            dataInfo = new DataInfo();
             // 压缩 2字节
-            info.setCompress(Integer.parseInt(strHead.substring(0, 2), 16));
+            dataInfo.setCompress(Integer.parseInt(strHead.substring(0, 2), 16));
             // 长度 10字节
-            info.setLen(Integer.parseInt(strHead.substring(2, 12)));
+            dataInfo.setLen(Integer.parseInt(strHead.substring(2, 12)));
             byte[] tmp = new byte[len - DataInfo.LEN];
             System.arraycopy(data, DataInfo.LEN, tmp, 0, len - DataInfo.LEN);
             return tmp;
